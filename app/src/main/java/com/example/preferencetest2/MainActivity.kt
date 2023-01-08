a149a689e5d26851c6b34b255e68a27b61587888
SagawaYasumasa <feelingyas@gmail.com>
2023-01-08 21:30:16 +0900

    modify editText

--- app/src/main/java/com/example/preferencetest2/MainActivity.kt
+++ app/src/main/java/com/example/preferencetest2/MainActivity.kt
@@ -10,10 +10,8 @@ import com.example.preferencetest2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var common : Common
-    private lateinit var instance : MainActivity        // applicationContext取得用
     private lateinit var binding : ActivityMainBinding
     private lateinit var serverAddress : String         // WebApi Server address
-
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +20,6 @@ class MainActivity : AppCompatActivity() {
         common= getApplication() as Common
         common.mainActivity = this
 
-        instance = this                                 // applicationContext取得用
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         Log.d("MainActivity onCreate","onCreate")
@@ -35,17 +32,12 @@ class MainActivity : AppCompatActivity() {
     }
     override fun onResume() {
         super.onResume()
-        serverAddress =loadPreference(this.getInstance())
+        serverAddress =loadPreference(common.mainActivity)
         Log.d("MainActivity OnResume","serverAddress="+serverAddress)
-    }
-    // applicationContext取得用
-    public fun getInstance():MainActivity{
-        return instance
     }
 }
 fun loadPreference(context:MainActivity) :String{
     val pref = PreferenceManager.getDefaultSharedPreferences(context)
-//        val pref = PreferenceManager.getDefaultSharedPreferences(this)
     val address = pref.getString("ADDRESS","")
     return (address.toString())
 }
